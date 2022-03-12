2bace16a405b7acdc6139bbe20bbfb332e0775a8
MosQyy <akbarma2002@gmail.com>
2022-03-12 11:23:10 +0700

    trying navigation

--- movie_app/src/screens/DetailScreen.js
+++ movie_app/src/screens/DetailScreen.js
@@ -1,10 +1,18 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
+import Color from '../config/utils/color';
+
 const DetailScreen = () => {
   return (
-    <View>
-      <Text>DetailScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: Color.MAIN_COLOR,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Sorry! This screen is currently empty.</Text>
     </View>
   );
 };
--- movie_app/src/screens/HomeScreen.js
+++ movie_app/src/screens/HomeScreen.js
@@ -1,9 +1,9 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 import Color from '../config/utils/color';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
   return (
     <View
       style={{
@@ -13,6 +13,17 @@ const HomeScreen = () => {
         alignItems: 'center',
       }}>
       <Text>Home Screen</Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'blue',
+          padding: 20,
+          borderRadius: 10,
+        }}
+        onPress={() => {
+          navigation.navigate('Detail');
+        }}>
+        <Text>Press here to Detail Screen</Text>
+      </TouchableOpacity>
     </View>
   );
 };
