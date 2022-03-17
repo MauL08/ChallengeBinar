b418c2f8e62a346b9c90e0631a39a331a36cf66d
MosQyy <akbarma2002@gmail.com>
2022-03-17 16:33:39 +0700

    split list

--- movie_app/src/components/Loading.js
+++ movie_app/src/components/Loading.js
@@ -1,16 +1,30 @@
+import {StatusBar} from 'react-native';
 import React from 'react';
+import {useIsFocused} from '@react-navigation/native';
+
 import {HandAnimation} from '../assets';
 import Color from '../config/utils/color';
 import LottieView from 'lottie-react-native';
 
 const Loading = () => {
+  function LoadingScreenStatusBar() {
+    const focus = useIsFocused();
+
+    return focus ? (
+      <StatusBar backgroundColor={Color.BACKGROUND_COLOR} />
+    ) : null;
+  }
+
   return (
-    <LottieView
-      source={HandAnimation}
-      autoPlay
-      loop
-      style={{backgroundColor: Color.BACKGROUND_COLOR}}
-    />
+    <>
+      <LoadingScreenStatusBar />
+      <LottieView
+        source={HandAnimation}
+        autoPlay
+        loop
+        style={{backgroundColor: Color.BACKGROUND_COLOR}}
+      />
+    </>
   );
 };
 
